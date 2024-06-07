4ffb580d5d8ee8c4ecec3c517613a4ca366e62dd
Borja <164376820+bmunoz-c@users.noreply.github.com>
2024-06-07 21:50:13 +0200

    Add files via upload

---
+++ level1/fizzbuzz.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmunoz-c <bmunoz-c@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 18:29:46 by bmunoz-c          #+#    #+#             */
+/*   Updated: 2024/06/07 19:04:10 by bmunoz-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int n)
+{
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+	{
+		n += '0';
+		write(1, &n, 1);
+	}
+}
+
+int	main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		i++;
+		write(1, "\n", 1);
+	}
+	return (0);
+}
